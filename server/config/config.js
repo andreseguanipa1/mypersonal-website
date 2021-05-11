22014a3d1773e988f9f6a73e5d7bf36af4a97844
Andres <andreseguanipa1@gmail.com>
2021-05-10 20:31:45 -0400

    v2.0.4

--- server/config/config.js
+++ server/config/config.js
@@ -4,11 +4,13 @@ Puerto
 
 process.env.PORT = process.env.PORT || 3000;
 
+
 /* =========
 Entorno
 ========= */
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
 
 /* ====================
 BASE DE DATOS
